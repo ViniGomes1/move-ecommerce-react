75ad0197f3c32ff2ba4ae373b98a85672b787859
ViniGomes1 <vinig0014@gmail.com>
2026-07-07 23:43:26 -0300

    pagina de eventos

--- src/Components/EventCard/EventCard.jsx
+++ src/Components/EventCard/EventCard.jsx
@@ -1,18 +1,18 @@
 import "./EventCard.css"
 
-function EventCard(){
+function EventCard({tipoEvento, endereco, data, hora, imagem}){
     return(
-        <div className="event-card-container">
+        <div className="event-card-container m-[10px]">
             <div className="absolute z-2">
                 <div className="pl-[20px] pt-[10px]">
-                    <p className="text-[#ffffff] text-[20px] font-bold">Corrida Social</p>
-                    <h4 className="mt-[10px] text-[#ffffff]">Avenida Atlântica, 110, Copacabana - Rio de Janeiro</h4>
-                    <h4 className="text-[15px]  mt-[25px] text-[#ffffff]">1 de Agosto</h4>
-                    <h4 className="mt-[5px] text-[#ffffff]">15:30 - HORÁRIO DE BRASÍLIA</h4>
+                    <p className="text-[#ffffff] text-[20px] font-bold">{tipoEvento}</p>
+                    <h4 className="mt-[10px] text-[#ffffff]">{endereco}</h4>
+                    <h4 className="text-[15px]  mt-[25px] text-[#ffffff]">{data}</h4>
+                    <h4 className="mt-[5px] text-[#ffffff]">{hora}</h4>
                 </div>
             </div>
             <div className="z-1">
-                <img className="object-cover object-[0px_-60px] opacity-50" src="/Imagens/Card Event/corrida.jpg" />
+                <img className="object-cover object-[0px_-60px] opacity-50" src={imagem} />
             </div>
         </div>
     )
--- src/Components/EventGrid/EventGrid.jsx
+++ src/Components/EventGrid/EventGrid.jsx
@@ -1,10 +1,49 @@
 import EventCard from "../EventCard/EventCard"
 import "./EventGrid.css"
 
+
+const eventoInfosList = [
+    {
+        tipoEvento: "Corrida Social",
+        endereco: "Avenida Atlântica, 110, Copacabana - Rio de Janeiro",
+        data: "1 de Agosto",
+        hora: "15:30 - HORÁRIO DE BRASÍLIA",
+        imagem: "/Imagens/Card Event/corrida.jpg"
+    },
+    {
+        tipoEvento: "Corrida",
+        endereco: "Avenida Atlântica, 110, Copacabana - Rio de Janeiro",
+        data: "1 de Agosto",
+        hora: "15:30 - HORÁRIO DE BRASÍLIA",
+        imagem: "/Imagens/Card Event/corrida.jpg"
+    },
+    {
+        tipoEvento: "Corrida",
+        endereco: "Avenida Atlântica, 110, Copacabana - Rio de Janeiro",
+        data: "1 de Agosto",
+        hora: "15:30 - HORÁRIO DE BRASÍLIA",
+        imagem: "/Imagens/Card Event/corrida.jpg"
+    },
+    {
+        tipoEvento: "Corrida",
+        endereco: "Avenida Atlântica, 110, Copacabana - Rio de Janeiro",
+        data: "1 de Agosto",
+        hora: "15:30 - HORÁRIO DE BRASÍLIA",
+        imagem: "/Imagens/Card Event/corrida.jpg"
+    }
+]
+
 function EventGrid(){
     return(
-        <div className="event-grid-container">
-            <EventCard />
+        <div className="p-[20px]">
+            <div className="mt-[30px] mb-[10px] flex items-center">
+                <h1 className="text-[20px] text-[#15342d] font-bold tracking-[2.5px]">PRÓXIMOS EVENTOS</h1>
+            </div>
+            <div className="flex flex-row flex-wrap">
+                {eventoInfosList.map((infos, index) => (
+                    <EventCard data={infos.data} endereco={infos.endereco} hora={infos.hora} imagem={infos.imagem} tipoEvento={infos.tipoEvento}/>
+                ))}
+            </div>
         </div>
     )
 }
